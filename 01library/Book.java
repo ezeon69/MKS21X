82363545a241788ff28af30919e0aa2e286e3dac
Jiajun Luo <luo.jiajun.jiajun@gmail.com>
2016-11-30 22:30:18 -0500

    finished book.java, tested library.java

--- 01library/Book.java
+++ 01library/Book.java
@@ -41,8 +41,8 @@ public class Book{
 	return title + ", " +  author + ", " + ISBN;
     }
 }
-
-    abstract Class LibraryBook extends Book implements Comparable<LibraryBook>{
+//librarybook
+abstract class LibraryBook extends Book implements Comparable<LibraryBook>{
 	
     private String callNumber;
 
@@ -63,20 +63,105 @@ public class Book{
 
     abstract void returned();
 
-    abstract String circulationStatus(){
-
-    }
+    abstract String circulationStatus();
     
     public int compareTo(LibraryBook x){
 	return callNumber.compareTo(x.getCallNumber());
     }
 
     public String toString(){
-	return super.toString() + circulationStatus() + callNumber;
+	return super.toString() + ", " + circulationStatus() + ", " + callNumber + "\n";
+    }
+    
+    }
+//referencebook
+    class ReferenceBook extends LibraryBook {
+
+    String collection;
+
+    public ReferenceBook(String author, String title, String ISBN, String callNumber, String list){
+        super(author,title,ISBN,callNumber);
+	collection = list;
     }
 
+    public String getCollection(){
+	return collection;
+    }
+
+    public void setCollection(String list){
+        collection = list;
+    }
+
+    public void checkout(String patron, String due){
+	System.out.println("cannot check out a reference book.");
+    }
+
+    public void returned(){
+	System.out.println("reference book could not have been checked out -- return impossible");
+    }
+
+    public String circulationStatus(){
+	return "non-circulating reference book";
+    }
+
+    public String toString(){
+	return super.toString() + ", " + collection + "\n";
+    }
 
 }
+//circulatingbook
+class CirculatingBook extends LibraryBook {
 
+    String currentHolder;
+    String dueDate;
+    String patron;
+
+    public CirculatingBook(String author, String title, String ISBN, String callNumber){
+	super(author,title,ISBN,callNumber);
+	currentHolder = null;
+	dueDate = null;
+    }
+
+    public String getCurrentHolder(){
+	return currentHolder;
+    }
+
+    public String getDueDate(){
+	return dueDate;
+    }
+
+    public void setCurrentHolder(String currentHolder){
+	this.currentHolder = currentHolder;
+    }
+
+    public void setDueDate(String dueDate){
+	this.dueDate = dueDate;
+    }
+
+    public void checkout(String patron, String due){
+	this.patron = patron;
+	this.dueDate = due;
+    }
+
+    public void returned(){
+	currentHolder = null;
+	dueDate = null;
+    }
+
+    public String circulationStatus(){
+        if (currentHolder == null){
+	    return "book available on shelves";
+	} else {
+	    return "current holder: " + currentHolder + "\ndue date: "+dueDate;
+	}
+    }
+
+    public String toString(){
+	return super.toString() + ", " + circulationStatus() + "\n";
+    }
 }
+
+
+
+
     
